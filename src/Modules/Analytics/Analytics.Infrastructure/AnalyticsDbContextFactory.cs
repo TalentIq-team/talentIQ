eeb7dadc67f31761a06fd49df534c8f594b14f18
dhanu2004847-ux <dhanu2004847@gmail.com>
2026-07-09 23:11:31 +0530

    implement analytics and talent pool backend

---
+++ src/Modules/Analytics/Analytics.Infrastructure/AnalyticsDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Analytics.Infrastructure;
+
+public class AnalyticsDbContextFactory : IDesignTimeDbContextFactory<AnalyticsDbContext>
+{
+    public AnalyticsDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AnalyticsDbContext>();
+
+        optionsBuilder.UseSqlServer(
+       "Server=localhost,11433;Database=TalentIQ;User Id=sa;Password=TalentIq2026!Secure;TrustServerCertificate=True");
+        return new AnalyticsDbContext(optionsBuilder.Options);
+    }
+}
